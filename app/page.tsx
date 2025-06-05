bc070a6284aee37bb19c989e9f1841ef34f8ced3
ThoomaasB <thomas.boucry@viacesi.fr>
2025-06-05 11:33:12 +0200

    prod and test

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js my best friend !</a>
+          Welcome to <a href="https://nextjs.org">Next.js my best code !</a>
         </h1>
 
         <p className={styles.description}>
